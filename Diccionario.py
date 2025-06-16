4ead088ff83750de430fbc6f8b3bfaeadc062f55
POAPCA02LC0126 <POAPCA01LC0400@POAPCA02LC0126.duoc.acad>
2025-06-16 14:18:42 -0400

    Clase tuplas y diccionario

---
+++ Diccionario.py
@@ -0,0 +1,11 @@
+diccionario = {
+    "rut": "22420501-5",
+    "nombre": "Matias Nuñez",
+    "direccion": "plaza oeste",
+    "fonos": [
+        997897969,
+        968695854,
+        947392748],
+    "activo": True}
+
+print(diccionario)
---
+++ Tuplas.py
@@ -0,0 +1,5 @@
+#Convertir una lista en tupla
+mi_lista = [1, 2, 3]
+mi_tupla = tuple(mi_lista)
+#Imprimir la tupla
+print(mi_tupla)
